4d6790eb517a0ce57407b426eb06156d1dd00f54
sebastianogange <sebastianogange@gmail.com>
2026-04-13 18:50:17 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,20 +1,12 @@
 import telebot
 import os
 
-# Prende le variabili da Railway
 TELEGRAM_TOKEN = os.getenv("8609230078:AAFCoriuwxzpAheNQMKZWZYbttR7aD_NUk")
-CHAT_ID = os.getenv("168842957")
+CHAT_ID = int(os.getenv("168842957"))
 
-# Crea il bot
 bot = telebot.TeleBot(TELEGRAM_TOKEN)
 
-# Funzione invio messaggio
-def send_message(msg):
-    bot.send_message(CHAT_ID, msg)
+bot.send_message(CHAT_ID, "✅ TEST OK - BOT FUNZIONA")
 
-# 🚀 TEST IMMEDIATO
-send_message("✅ BOT ONLINE E COLLEGATO CORRETTAMENTE")
-
-# Mantiene il bot attivo
 while True:
     pass
